e8d78c67e0c73613ee94adeb75cc8645f7e68d3d
Teemu Lehtinen <teemu@debyte.fi>
2024-11-21 20:41:05 +0200

    Render class attributes and singleton tags for tsx

--- src/render.ts
+++ src/render.ts
@@ -61,11 +61,22 @@ const ATTR_TYPES = new Map<string, string>([
 
 const renderDOM = (template: string) => {
   return template
-    .replace(/"{tsx:(\w+)}"/g, "{$1}")
-    .replace(/data-tsx-map="(\w+)"/g, "{...$1}")
-    .replace(/<div data-tsx-cond="(\w+)"><\/div>/g, "{$1 && (")
-    .replace(/<div data-tsx-cond=""><\/div>/g, ")}")
+    .replace(/"{tsx:(\w+)}"/gi, "{$1}")
+    .replace(/data-tsx-map="(\w+)"/gi, "{...$1}")
+    .replace(/<div data-tsx-cond="(\w+)"><\/div>/gi, "{$1 && (")
+    .replace(/<div data-tsx-cond=""><\/div>/gi, ")}")
+    .replace(/ class="([^"]*)"/gi, " className=\"$1\"")
+    .replace(closeTagsRegexp, "<$1$2/>");
 };
+
+const singletonTags = [
+  "area", "base", "br", "col", "command", "embed", "hr", "img", "input",
+  "keygen", "link", "meta", "param", "source", "track", "wbr",
+];
+
+const closeTagsRegexp = new RegExp(
+  `<(${singletonTags.join("|")})( ([^>]*[^/])?)?>`, "gi"
+);
 
 const indentRows = (src: string) => {
   const rows = src.replace("\t", "  ").split("\n");
--- tests/render.test.ts
+++ tests/render.test.ts
@@ -1,16 +1,13 @@
-import { beforeAll, expect, test } from "@jest/globals";
+import { expect, test } from "@jest/globals";
 import { DocPool } from "../src/data/DocPool";
 import { selectParser } from "../src/parse";
 import { renderFC } from "../src/render";
 import { getReadmeHtmlExample } from "./helpers";
 
-const docs = new DocPool();
-
-beforeAll(async () => {
-  docs.source = { type: "string", data: await getReadmeHtmlExample() };
-});
-
 test("Should render React.FC from detected components", async () => {
+  const docs = new DocPool(
+    { type: "string", data: await getReadmeHtmlExample() }
+  );
   const parser = selectParser(docs, "custom");
   for (const component of await parser.getComponents()) {
     const out = renderFC(component);
@@ -27,6 +24,20 @@ test("Should render React.FC from detected components", async () => {
       expect(out.match(/code: number/)).not.toBeNull();
       expect(out.match(/<img [^>]*src={image}/)).not.toBeNull();
       expect(out.match(/<span>{code}<\/span>/)).not.toBeNull();
+    }
+  }
+});
+
+test("Should format class names and singleton tags for tsx", async () => {
+  const docs = new DocPool({ type: "file", data: "tests/webflowish.html" });
+  const parser = selectParser(docs, "webflow/export");
+  for (const component of await parser.getComponents()) {
+    const out = renderFC(component);
+    if (component.name === "Testimonial") {
+      expect(out.match(/<h3 className="testimonial-main-heading">/))
+        .not.toBeNull();
+      expect(out.match(/<hr\/>/)).not.toBeNull();
+      expect(out.match(/<img [^>]+\/>/)).not.toBeNull();
     }
   }
 });
