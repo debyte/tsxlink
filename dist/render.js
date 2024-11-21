91b92d8998274aeced5f631d48e95a049071a212
Teemu Lehtinen <teemu@debyte.fi>
2024-11-21 14:56:42 +0200

    Fix components having less than two rows

--- dist/render.js
+++ dist/render.js
@@ -63,22 +63,26 @@ const renderDOM = (template) => {
 };
 const indentRows = (src) => {
     const rows = src.replace("\t", "  ").split("\n");
-    const ind = getIndent(rows[0]);
-    if (ind > 2) {
-        rows[0] = rows[0].substring(ind - 2);
-    }
-    else if (ind < 2) {
-        rows[0] = addIndent(rows[0], 2 - ind);
-    }
-    const nInd = getIndent(rows[1]);
-    if (nInd > 4) {
-        for (let i = 1; i < rows.length; i++) {
-            rows[i] = rows[i].substring(nInd - 4);
+    if (rows.length > 0) {
+        const ind = getIndent(rows[0]);
+        if (ind > 2) {
+            rows[0] = rows[0].substring(ind - 2);
         }
-    }
-    else if (nInd < 4) {
-        for (let i = 1; i < rows.length; i++) {
-            rows[i] = addIndent(rows[i], 4 - nInd);
+        else if (ind < 2) {
+            rows[0] = addIndent(rows[0], 2 - ind);
+        }
+        if (rows.length > 1) {
+            const nInd = getIndent(rows[1]);
+            if (nInd > 4) {
+                for (let i = 1; i < rows.length; i++) {
+                    rows[i] = rows[i].substring(nInd - 4);
+                }
+            }
+            else if (nInd < 4) {
+                for (let i = 1; i < rows.length; i++) {
+                    rows[i] = addIndent(rows[i], 4 - nInd);
+                }
+            }
         }
     }
     return rows.join("\n");
--- src/render.ts
+++ src/render.ts
@@ -69,20 +69,24 @@ const renderDOM = (template: string) => {
 
 const indentRows = (src: string) => {
   const rows = src.replace("\t", "  ").split("\n");
-  const ind = getIndent(rows[0]);
-  if (ind > 2) {
-    rows[0] = rows[0].substring(ind - 2);
-  } else if (ind < 2) {
-    rows[0] = addIndent(rows[0], 2 - ind);
-  }
-  const nInd = getIndent(rows[1]);
-  if (nInd > 4) {
-    for (let i = 1; i < rows.length; i++) {
-      rows[i] = rows[i].substring(nInd - 4);
+  if (rows.length > 0) {
+    const ind = getIndent(rows[0]);
+    if (ind > 2) {
+      rows[0] = rows[0].substring(ind - 2);
+    } else if (ind < 2) {
+      rows[0] = addIndent(rows[0], 2 - ind);
     }
-  } else if (nInd < 4) {
-    for (let i = 1; i < rows.length; i++) {
-      rows[i] = addIndent(rows[i], 4 - nInd);
+    if (rows.length > 1) {
+      const nInd = getIndent(rows[1]);
+      if (nInd > 4) {
+        for (let i = 1; i < rows.length; i++) {
+          rows[i] = rows[i].substring(nInd - 4);
+        }
+      } else if (nInd < 4) {
+        for (let i = 1; i < rows.length; i++) {
+          rows[i] = addIndent(rows[i], 4 - nInd);
+        }
+      }
     }
   }
   return rows.join("\n");
